3c88d935c50291ee9702caeb076c7c139ed9a756
Dan Leonardi <danleon89@gmail.com>
2025-11-20 00:22:54 -0300

    Código organizado por html, css e Js.

---
+++ js/script.js
@@ -0,0 +1,11 @@
+function irParaGaleria() {
+  window.location.href = 'galeria.html';
+}
+
+function irParaContato() {
+  window.location.href = 'contato.html';
+}
+
+function irParaMaterial() {
+  window.location.href = 'material.html';
+}
